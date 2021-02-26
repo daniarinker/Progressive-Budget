3ab5d1ea24703c141f048acf3dc4adbd97870f04
Dania Rinker <daniarinker@gmail.com>
2021-02-26 03:58:28 -0800

    added files_to_cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,16 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+const FILES_TO_CACHE = [
+  "/",
+  "/index.js",
+  "/style.css",
+  "/service-worker.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
 const iconSizes = ["72", "96", "128", "144", "152", "192", "384", "512"];
 const iconFiles = iconSizes.map(
   (size) => `/assets/images/icons/icon-${size}x${size}.png`
